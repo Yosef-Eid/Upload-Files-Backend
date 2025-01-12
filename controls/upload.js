fea59c3a536903d717ff85b359d38f9f14558728
joscript <yosefeid058@gmail.com>
2025-01-12 20:09:22 +0200

    upload-files

--- controls/upload.js
+++ controls/upload.js
@@ -1,26 +1,31 @@
 import { MultipleUpload, Upload } from '../model/uploadSchema.js'
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
 
 export const upload = async (req, res) => {
+    if (!req.file) return res.status(400).json({ message: "No file uploaded" });
 
-    if (!req.file) {
-        return res.status(400).json({ message: "No file uploaded" });
-    }
     try {
-        const result = await new Upload({ image: req.file.filename }).save()
-        res.status(201).json(result)
+        const result = await cloudinary.uploader.upload(req.file.path, { folder: 'upload' })
+        fs.unlinkSync(req.file.path)
+        const image = await new Upload({ image: result.secure_url }).save()
+        res.status(201).json(image)
     } catch (error) {
         res.status(400).json({ message: error.message })
     }
 }
 
 export const mutuleUpload = async (req, res) => {
+    if (!req.files || req.files.length === 0) return res.status(400).json({ message: "No files uploaded" });
+    
     try {
-        if (!req.files || req.files.length === 0) return res.status(400).json({ message: "No files uploaded" });
-
-        const images = req.files.map(file => file.filename)
-
-        const result = await new MultipleUpload({images}).save();
-        res.status(201).json(result)
+        const files = req.files.map(file => file.path)
+        const file = files.map(async file => await cloudinary.uploader.upload(file, { folder: 'upload' }))
+        const result = await Promise.all(file)
+        req.files.forEach(file => fs.unlinkSync(file.path));
+        const images = result.map(result => result.secure_url);
+        const savedImages = await new MultipleUpload({ images }).save();
+        res.status(201).json(savedImages)
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
--- utils/uploadHandler.js
+++ utils/uploadHandler.js
@@ -1,13 +1,22 @@
+import { v2 as cloudinary } from 'cloudinary';
 import multer from 'multer'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import dotenv from 'dotenv';
+dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+})
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../images'))
+        cb(null, path.join(__dirname, '../upload'))
     },
     filename: function (req, file, cb) {
         const ext = path.extname(file.originalname)
@@ -17,15 +26,12 @@ const storage = multer.diskStorage({
 })
 
 const filterImage = (req, file, cb) => {
-    const fileTypes = /jpg|png|jpeg/;
+    const fileTypes = /jpg|png|jpeg|gif/;
     const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
     const mimetype = fileTypes.test(file.mimetype);
 
-    if (extname && mimetype) {
-        cb(null, true)
-    } else {
-        cb(new Error("Only images are allowed"))
-    }
+    if (extname && mimetype) cb(null, true)
+    else cb(new Error("Only images are allowed"))
 }
 
 export const uploadFiles = multer({
